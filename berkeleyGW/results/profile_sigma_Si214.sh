4537e72d815ad35a417982fc0b57ee6ed45c2f0a
csruiliu <rui.liu@proton.me>
2025-03-16 17:48:17 -0700

    update sigma script

--- berkeleyGW/results/profile_sigma_Si214.sh
+++ berkeleyGW/results/profile_sigma_Si214.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 #SBATCH -t 00:30:00
-#SBATCH --qos=regular
+##SBATCH --qos=regular
 #SBATCH -N 1
 #SBATCH --gpus-per-node=4
 #SBATCH -A nstaff
-#SBATCH -J bgw_eps_Si214
-#SBATCH -C gpu
-#SBATCH -o BGW_EPSILON_%j.out
+#SBATCH -J bgw_sig_Si214
+#SBATCH  -C gpu
+#SBATCH -o BGW_SIGMA_%j.out
 # #SBATCH --reservation=n10scaling
 
 source ../site_path_config.sh
 
-mkdir BGW_SIGMA_$SLURM_JOBID
-stripe_large BGW_SIGMA_$SLURM_JOBID
-cp ./wrap_dcgmi.sh BGW_SIGMA_$SLURM_JOBID
-cd    BGW_SIGMA_$SLURM_JOBID
+mkdir BGW_SIGMA_$SLURM_JOB_ID
+stripe_large BGW_SIGMA_$SLURM_JOB_ID
+cp ./wrap_dcgmi.sh BGW_SIGMA_$SLURM_JOB_ID
+cd    BGW_SIGMA_$SLURM_JOB_ID
+
 ln -s $BGW_DIR/sigma.cplx.x .
-ln -s  ../sigma.inp .
-ln -sfn  ${Si214_WFN_folder}/WFNq.h5      .
-ln -sfn  ${Si214_WFN_folder}/WFN_out.h5   ./WFN.h5
+NNPOOL=2
+cat ../sigma.inp |\
+sed "s/NNPOOL/${NNPOOL}/g" > sigma.inp
+ln -sfn  ${Si214_WFN_folder}/WFN_out.h5   ./WFN_inner.h5
+ln -sfn  ${Si214_WFN_folder}/RHO          .
+ln -sfn  ${Si214_WFN_folder}/VXC          .
+ln -sfn  ${Si214_WFN_folder}/eps0mat.h5   .
 
 
 ulimit -s unlimited
@@ -32,8 +37,7 @@ export OMP_NUM_THREADS=16
 export DCGM_SAMPLE_RATE=100
 
 start=$(date +%s.%N)
-
-dcgm_delay=${DCGM_SAMPLE_RATE} srun -N 1 -n 4 -c 32 --cpu-bind=cores wrap_dcgmi.sh ./sigma.cplx.x
+dcgm_delay=${DCGM_SAMPLE_RATE} srun -n 4 -c 32 --cpu-bind=cores wrap_dcgmi.sh ./sigma.cplx.x
 
 end=$(date +%s.%N)
 elapsed=$(printf "%s - %s\n" $end $start | bc -l)
