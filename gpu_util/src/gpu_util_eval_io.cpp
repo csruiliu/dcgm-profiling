fbf7b3e995c93cf46c333a82ee34ddcae78dfb09
csruiliu <rui.liu@proton.me>
2025-03-18 15:43:47 -0700

    add computing average pcie bandwidth

--- gpu_util/src/gpu_util_eval_io.cpp
+++ gpu_util/src/gpu_util_eval_io.cpp
@@ -38,10 +38,17 @@ int main(int argc, char *argv[])
     return -1;
   }
 
+  double total_time = 0.0;
+
   // Copy data from host to device multiple times
   for (int i = 0; i < NUM_COPIES; ++i)
   {
+    auto start = std::chrono::high_resolution_clock::now();
+    
     err = cudaMemcpy(x_d, x_h, size, cudaMemcpyHostToDevice);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
     if (err != cudaSuccess)
     {
       std::cerr << "Failed to copy memory from host to device: " << cudaGetErrorString(err) << std::endl;
@@ -49,7 +56,21 @@ int main(int argc, char *argv[])
       free(x_h);
       return -1;
     }
+
+    std::chrono::duration<double> elapsed = end - start;
+    double copy_time = elapsed.count();
+    total_time += copy_time;
+
+    // Optional: Print bandwidth for each individual copy
+    double bandwidth = (size / (1024.0 * 1024.0 * 1024.0)) / copy_time; // GiB/s
+    std::cout << "Copy " << i << " bandwidth: " << bandwidth << " GiB/s" << std::endl;
   }
+
+  // Calculate average bandwidth
+  double total_data = NUM_COPIES * (size / (1024.0 * 1024.0 * 1024.0)); // Total data in GiB
+  double avg_bandwidth = total_data / total_time; // GiB/s
+
+  std::cout << "Average PCIe bandwidth: " << avg_bandwidth << " GiB/s" << std::endl;
 
   // Free device memory
   cudaFree(x_d);
