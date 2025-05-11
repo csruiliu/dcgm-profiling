0a20db5462c1d8898fbc1f100f5b5894a1fa174f
csruiliu <rui.liu@proton.me>
2025-05-10 18:33:09 -0700

    refine the source code for gemm and cal_gemm

--- gemm/src/calc_gemm.cpp
+++ gemm/src/calc_gemm.cpp
@@ -1,9 +1,9 @@
 
-#if (PRECISION == 1)
+#if (PRECISION == 'S')
 #define gemm_t float
 #define gemm_f cublasSgemm
 #define cumode CUBLAS_TF32_TENSOR_OP_MATH
-#elif (PRECISION == 2)
+#elif (PRECISION == 'D')
 #define gemm_t double
 #define gemm_f cublasDgemm
 #define cumode CUBLAS_DEFAULT_MATH
@@ -16,23 +16,16 @@
 #endif
 
 static inline void
-alloc_gemm(int N,
-           gemm_t **pmatrixA,
-           gemm_t **pmatrixB,
-           gemm_t **pmatrixC)
-{
-
-  mprintf("Allocating Matrices...\n");
+alloc_gemm(int N, gemm_t **pmatrixA, gemm_t **pmatrixB, gemm_t **pmatrixC) {
+  printf("Allocating Matrices...\n");
   gemm_t *__restrict__ matrixA = (gemm_t *)malloc(sizeof(gemm_t) * N * N);
   gemm_t *__restrict__ matrixB = (gemm_t *)malloc(sizeof(gemm_t) * N * N);
   gemm_t *__restrict__ matrixC = (gemm_t *)malloc(sizeof(gemm_t) * N * N);
 
-  mprintf("Allocation complete, populating with values...\n");
+  printf("Allocation complete, populating with values...\n");
 #pragma omp parallel for
-  for (int i = 0; i < N; i++)
-  {
-    for (int j = 0; j < N; j++)
-    {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
       matrixA[i * N + j] = drand48() - 0.5;
       matrixB[i * N + j] = drand48() - 0.5;
       matrixC[i * N + j] = drand48() - 0.5;
@@ -46,20 +39,14 @@ alloc_gemm(int N,
 }
 
 static inline long long int
-check_gemm(int N,
-           gemm_t *matrixA,
-           gemm_t *matrixB,
-           gemm_t *matrixC)
-{
-  mprintf("Calculating matrix check...\n");
+check_gemm(int N, gemm_t *matrixA, gemm_t *matrixB, gemm_t *matrixC) {
+  printf("Calculating matrix check...\n");
 
   // double final_sum = 0;
   long long int count = 0;
 #pragma omp parallel for reduction(+ : final_sum, count)
-  for (int i = 0; i < N; i++)
-  {
-    for (int j = 0; j < N; j++)
-    {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
       // final_sum += matrixC[i*N + j];
       // printf("matrixC[i*N + j] %f \n",__half2float(matrixC[i*N + j]));
       count++;
@@ -70,10 +57,7 @@ check_gemm(int N,
 }
 
 static inline void
-free_gemm(gemm_t *matrixA,
-          gemm_t *matrixB,
-          gemm_t *matrixC)
-{
+free_gemm(gemm_t *matrixA, gemm_t *matrixB, gemm_t *matrixC) {
   free(matrixA);
   free(matrixB);
   free(matrixC);
@@ -82,10 +66,9 @@ free_gemm(gemm_t *matrixA,
 
 static inline double
 calc_gemm(int repeats, int N, double dalpha, double dbeta,
-          gemm_t *matrixA, gemm_t *matrixB, gemm_t *matrixC)
-{
+          gemm_t *matrixA, gemm_t *matrixB, gemm_t *matrixC) {
 
-  mprintf("Performing multiplication...\n");
+  printf("Performing multiplication...\n");
 
   gemm_t alpha = dalpha;
   gemm_t beta = dbeta;
@@ -95,8 +78,7 @@ calc_gemm(int repeats, int N, double dalpha, double dbeta,
   errorA = cudaMalloc((void **)&d_matrixA, N * N * sizeof(gemm_t));
   errorB = cudaMalloc((void **)&d_matrixB, N * N * sizeof(gemm_t));
   errorC = cudaMalloc((void **)&d_matrixC, N * N * sizeof(gemm_t));
-  if ((errorA != cudaSuccess) || (errorB != cudaSuccess) || (errorC != cudaSuccess))
-  {
+  if (errorA != cudaSuccess || errorB != cudaSuccess || errorC != cudaSuccess) {
     printf("ERROR: allocating device matrices\n");
     exit(1);
   }
@@ -104,8 +86,7 @@ calc_gemm(int repeats, int N, double dalpha, double dbeta,
   cublasStatus_t status;
   cublasHandle_t handle;
   status = cublasCreate(&handle);
-  if (status != CUBLAS_STATUS_SUCCESS)
-  {
+  if (status != CUBLAS_STATUS_SUCCESS) {
     printf("ERROR: creating a device handle\n");
     exit(1);
   }
@@ -115,21 +96,18 @@ calc_gemm(int repeats, int N, double dalpha, double dbeta,
   statusA = cublasSetMatrix(N, N, sizeof(gemm_t), matrixA, N, d_matrixA, N);
   statusB = cublasSetMatrix(N, N, sizeof(gemm_t), matrixB, N, d_matrixB, N);
   statusC = cublasSetMatrix(N, N, sizeof(gemm_t), matrixC, N, d_matrixC, N);
-  if ((statusA != CUBLAS_STATUS_SUCCESS) || (statusB != CUBLAS_STATUS_SUCCESS) || (statusC != CUBLAS_STATUS_SUCCESS))
-  {
+  if (statusA != CUBLAS_STATUS_SUCCESS || statusB != CUBLAS_STATUS_SUCCESS || statusC != CUBLAS_STATUS_SUCCESS) {
     printf("ERROR: intializing device matrices\n");
     exit(1);
   }
 
   // Repeat multiple times
   const double start = get_seconds();
-  for (int r = 0; r < repeats; r++)
-  {
+  for (int r = 0; r < repeats; r++) {
     cublasStatus_t matmulStatus;
     matmulStatus = gemm_f(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N,
                           &alpha, d_matrixA, N, d_matrixB, N, &beta, d_matrixC, N);
-    if (matmulStatus != CUBLAS_STATUS_SUCCESS)
-    {
+    if (matmulStatus != CUBLAS_STATUS_SUCCESS) {
       printf("MatMul Function failed with error %d\n", matmulStatus);
       exit(1);
     }
@@ -149,4 +127,3 @@ calc_gemm(int repeats, int N, double dalpha, double dbeta,
 #undef gemm_t
 #undef gemm_f
 #undef cumode
-
--- gemm/src/gemm.cpp
+++ gemm/src/gemm.cpp
@@ -7,40 +7,24 @@
 #include <cublasLt.h>
 #include <cuda_fp16.h>
 
-#ifdef USE_MPI
-#include <mpi.h>
-#define mprintf if( mpi_rank==0 ) printf
-#else
-#define mprintf printf
-#endif
-int mpi_thread=0;
-int mpi_rank=0;
-int mpi_size=1;
-
-
 // ------------------------------------------------------- //
 // Function: get_seconds
 // ------------------------------------------------------- //
 double get_seconds() {
-
-#ifdef USE_MPI
-  MPI_Barrier( MPI_COMM_WORLD );
-#endif
-
   struct timeval now;
   gettimeofday(&now, NULL);
 
-  const double seconds = (double) now.tv_sec;
-  const double usec    = (double) now.tv_usec;
+  const double seconds = (double)now.tv_sec;
+  const double usec = (double)now.tv_usec;
 
   return seconds + (usec * 1.0e-6);
 }
 
-#define PRECISION 1
+#define PRECISION 'S'
 #include "calc_gemm.cpp"
 #undef PRECISION
 
-#define PRECISION 2
+#define PRECISION 'D'
 #include "calc_gemm.cpp"
 #undef PRECISION
 
@@ -48,130 +32,112 @@ double get_seconds() {
 #include "calc_gemm.cpp"
 #undef PRECISION
 
+int main(int argc, char *argv[]) {
 
-int main(int argc, char* argv[]) {
-
-  #ifdef USE_MPI
-  MPI_Init_thread( &argc, &argv, MPI_THREAD_FUNNELED, &mpi_thread );
-  MPI_Comm_size( MPI_COMM_WORLD, &mpi_size );
-  MPI_Comm_rank( MPI_COMM_WORLD, &mpi_rank );
-  #endif
-  
   int N = 4096;
   int repeats = 100;
   double alpha = 1.0;
-  double beta  = 1.0;
+  double beta = 1.0;
   char prec = 'D';
 
-  if( true ){//argv parse
-
-    // argv[1] is the matrix size
-    if (argc > 1) {
-      N = atoi(argv[1]);
-      mprintf("Matrix size input by command line: %d\n", N);
-    }else{
-      mprintf("Matrix size defaulted to %d\n", N);
-    }
-    
-    // argv[2] is the number of trials
-    if (argc > 2) {
-      repeats = atoi(argv[2]);
-      mprintf("Repeat multiply %d times.\n", repeats);
-    }else{
-      mprintf("Repeat multiply defaulted to %d\n", repeats);
-    }
-    
-    // argv[3] is alpha
-    if (argc > 3) {
-      alpha = atof(argv[3]);
-    }
-    mprintf("Alpha =    %f\n", alpha);
-    
-    // argv[4] is beta
-    if (argc > 4) {
-      beta = atof(argv[4]);
-    }
-    mprintf("Beta  =    %f\n", beta);
-    
-    // argv[5] is precision
-    if (argc > 5 ){
-      if(      argv[5][0] == 'S' ) prec='S';
-      else if( argv[5][0] == 'D' ) prec='D';
-      else if( argv[5][0] == 'H' ) prec='H';
-      else {
-	mprintf("Reqested precision (%s) not recognized. "
-		"Using default (%c).\n", argv[5], prec );
-      } }
-    else{
-      mprintf("Precision defaulted to %c\n", prec );
-    }
-    mprintf("Precision =    %c\n", prec );
-      
+  // ------------------------------------------------------- //
+  // Arguments Parsing
+  // ------------------------------------------------------- //
+  // argv[1] is the matrix size
+  if (argc > 1) {
+    N = atoi(argv[1]);
+    printf("Matrix size input by command line: %d\n", N);
+  } else {
+    printf("Matrix size defaulted to %d\n", N);
   }
+
+  // argv[2] is the number of trials
+  if (argc > 2) {
+    repeats = atoi(argv[2]);
+    printf("Repeat multiply %d times.\n", repeats);
+  } else {
+    printf("Repeat multiply defaulted to %d\n", repeats);
+  }
+
+  // argv[3] is alpha
+  if (argc > 3) {
+    alpha = atof(argv[3]);
+  }
+  printf("Alpha =    %f\n", alpha);
+
+  // argv[4] is beta
+  if (argc > 4) {
+    beta = atof(argv[4]);
+  }
+  printf("Beta  =    %f\n", beta);
+
+  // argv[5] is precision
+  if (argc > 5) {
+    if (argv[5][0] == 'S')
+      prec = 'S';
+    else if (argv[5][0] == 'D')
+      prec = 'D';
+    else if (argv[5][0] == 'H')
+      prec = 'H';
+    else {
+      printf("Reqested precision (%s) not recognized. Using default (%c).\n", argv[5], prec);
+    }
+  } else {
+    printf("Precision defaulted to %c\n", prec);
+  }
+  printf("Precision =    %c\n", prec);
 
   double time_taken;
   int sizeof_gemm_t;
   int status;
-  switch (prec ){
-  case('S'):{
-    float *matrixA, *matrixB, *matrixC;
-    alloc_gemm( N, &matrixA, &matrixB, &matrixC );
-    time_taken = calc_gemm( repeats, N, alpha, beta, matrixA, matrixB, matrixC );
-    status = check_gemm( N, matrixA, matrixB, matrixC );
-    free_gemm( matrixA, matrixB, matrixC );
-    sizeof_gemm_t = sizeof( float );
-    break;
-  }
-  case('D'):{
-    double *matrixA, *matrixB, *matrixC;
-    alloc_gemm( N, &matrixA, &matrixB, &matrixC );
-    time_taken = calc_gemm( repeats, N, alpha, beta, matrixA, matrixB, matrixC );
-    status = check_gemm( N, matrixA, matrixB, matrixC );
-    free_gemm( matrixA, matrixB, matrixC );
-    sizeof_gemm_t = sizeof( double );
-    break;
-  }
-  case('H'):{
-    __half *matrixA, *matrixB, *matrixC;
-    alloc_gemm( N, &matrixA, &matrixB, &matrixC );
-    time_taken = calc_gemm( repeats, N, alpha, beta, matrixA, matrixB, matrixC );
-    status = check_gemm( N, matrixA, matrixB, matrixC );
-    free_gemm( matrixA, matrixB, matrixC );
-    sizeof_gemm_t = sizeof( __half );
-    break;
-  }
-  }
 
-  
-
-  if( true ){ // Print results
-
-    mprintf("\n");
-    mprintf("===============================================================\n");
-
-    double N_dbl = (double) N;
-    double matrix_memory = (3 * N_dbl * N_dbl) * ((double) sizeof_gemm_t);
-    
-    mprintf("Memory for Matrices:  %f MB\n", (matrix_memory / (1024 * 1024)));
-    
-    mprintf("Multiply time:        %f seconds\n", time_taken);
-    
-    const double flops_computed =
-      ( (N_dbl * N_dbl * N_dbl * 2.0 * (double)(repeats)) +
-	(N_dbl * N_dbl * 3 * (double)(repeats)) ) * (double)(mpi_size);
-    
-    //mprintf("FLOPs computed:       %f\n", flops_computed);
-    mprintf("GFLOP/s rate:         %f GF/s\n",
-	    (flops_computed / time_taken) / 1.0e9);
-    
-    mprintf("===============================================================\n");
-    mprintf("\n");
-
+  switch (prec) {
+    case 'S': {
+      float *matrixA, *matrixB, *matrixC;
+      alloc_gemm(N, &matrixA, &matrixB, &matrixC);
+      time_taken = calc_gemm(repeats, N, alpha, beta, matrixA, matrixB, matrixC);
+      status = check_gemm(N, matrixA, matrixB, matrixC);
+      free_gemm(matrixA, matrixB, matrixC);
+      sizeof_gemm_t = sizeof(float);
+      break;
+    }
+    case 'D': {
+      double *matrixA, *matrixB, *matrixC;
+      alloc_gemm(N, &matrixA, &matrixB, &matrixC);
+      time_taken = calc_gemm(repeats, N, alpha, beta, matrixA, matrixB, matrixC);
+      status = check_gemm(N, matrixA, matrixB, matrixC);
+      free_gemm(matrixA, matrixB, matrixC);
+      sizeof_gemm_t = sizeof(double);
+      break;
+    }
+    case 'H': {
+      __half *matrixA, *matrixB, *matrixC;
+      alloc_gemm(N, &matrixA, &matrixB, &matrixC);
+      time_taken = calc_gemm(repeats, N, alpha, beta, matrixA, matrixB, matrixC);
+      status = check_gemm(N, matrixA, matrixB, matrixC);
+      free_gemm(matrixA, matrixB, matrixC);
+      sizeof_gemm_t = sizeof(__half);
+      break;
+    }
   }
 
-  #ifdef USE_MPI
-  MPI_Finalize();
-  #endif
-  
+  printf("\n");
+  printf("===============================================================\n");
+
+  double N_dbl = (double) N;
+  double matrix_memory = (3 * N_dbl * N_dbl) * ((double)sizeof_gemm_t);
+
+  printf("Memory for Matrices:  %f MB\n", (matrix_memory / (1024 * 1024)));
+
+  printf("Multiply time:        %f seconds\n", time_taken);
+
+  const double flops_computed = (N_dbl * N_dbl * N_dbl * 2.0 * (double)(repeats)) + (N_dbl * N_dbl * 3 * (double)(repeats));
+
+  // mprintf("FLOPs computed:       %f\n", flops_computed);
+  printf("GFLOP/s rate:         %f GF/s\n", (flops_computed / time_taken) / 1.0e9);
+
+  printf("===============================================================\n");
+  printf("\n");
+
   return 0;
 }
