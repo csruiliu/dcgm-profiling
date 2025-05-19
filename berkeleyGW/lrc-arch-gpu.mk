ea07457d48bd9fa94411a802b46cdf3d966db618
csruiliu <rui.liu@proton.me>
2025-05-19 09:00:56 -0700

    add lrc-arch-gpu.mk

--- berkeleyGW/lrc-arch-gpu.mk
+++ berkeleyGW/lrc-arch-gpu.mk
@@ -1,44 +1,81 @@
-# This compilation file is still under test
-
+# arch.mk for BerkeleyGW codes
+#
+# suitable for Perlmutter
+#
+# MDB
+# 2021, Perlmutter@NERSC
+# nvidia compiler
+# 
+# Do:
+#
+# module swap PrgEnv-gnu PrgEnv-nvhpc ; module load cray-hdf5-parallel ; module load cray-fftw ; module load cray-libsci ; module load python
+#
+#
 COMPFLAG  = -DPGI
 PARAFLAG  = -DMPI  -DOMP
 MATHFLAG  = -DUSESCALAPACK -DUNPACKED -DUSEFFTW3 -DHDF5  -DOPENACC -DOMP_TARGET # -DUSEELPA
+# DEBUGFLAG = -DDEBUG -DNVTX
+#
 
-NVCC=/global/software/rocky-8.x86_64/gcc/linux-rocky8-x86_64/gcc-13.2.0/cuda-12.6.0-pe26b3ktosinq7fckw2453oovahnbtui/bin/nvcc
-NVCCOPT= -O3 -use_fast_math
+NVCC=nvcc 
+NVCCOPT= -O2 -use_fast_math
+# CUDALIB=-L$(CUDA_DIR)/lib64/  -lcufft -lcublas  -lcudart  -lcuda  -lnvToolsExt
 CUDALIB= -lcufft -lcublasLt -lcublas -lcudart -lcuda
 
-FCPP    = /usr/bin/cpp  -C   -nostdinc
-F90free = mpifort -Mfree -acc -mp=multicore,gpu -gpu=cc80  -Mcudalib=cublas,cufft -Mcuda=lineinfo -traceback -Minfo=mp,acc -gopt -traceback
-LINK    = mpifort        -acc -mp=multicore,gpu -gpu=cc80  -Mcudalib=cublas,cufft -Mcuda=lineinfo -Minfo=mp,acc
-FOPTS   = -fast -Mfree -Mlarge_arrays
+FCPP    = /usr/bin/cpp  -C   -nostdinc   #  -C  -P  -E -ansi  -nostdinc  /usr/bin/cpp
+# F90free = ftn -Mfree -acc -mp  -Mcudalib=cublas,cufft -Mcuda=lineinfo -traceback -Minfo=mp
+# LINK    = ftn        -acc -mp  -Mcudalib=cublas,cufft -Minfo=mp
+# FOPTS   = -fast -Mfree -Mlarge_arrays 
+F90free = mpifort -Mfree -acc -mp=multicore,gpu -gpu=cc80  -Mcudalib=cublas,cufft -Mcuda=lineinfo -traceback -Minfo=mp,acc -gopt -traceback -tp=x86-64-v3 -O2
+LINK    = mpifort        -acc -mp=multicore,gpu -gpu=cc80  -Mcudalib=cublas,cufft -Mcuda=lineinfo -Minfo=mp,acc -tp=x86-64-v3 -O2
+FOPTS   = -O2 -Mfree -Mlarge_arrays -tp=x86-64-v3
 FNOOPTS = $(FOPTS)
 MOD_OPT = -module  
 INCFLAG = -I #./
 
 C_PARAFLAG  = -DPARA -DMPICH_IGNORE_CXX_SEEK
-CC_COMP = mpiCC
+CC_COMP = mpicxx
 C_COMP  = mpicc
-C_LINK  = mpicc -lstdc++ # ${CUDALIB} -lstdc++
-C_OPTS  = -O3 -fopenmp 
-#C_OPTS  = -fast -mp 
+C_LINK  = mpicxx -lstdc++ # ${CUDALIB} -lstdc++
+C_OPTS  = -O2 -mp -tp=x86-64-v3
+C_DEBUGFLAG =
 
 REMOVE  = /bin/rm -f
 
-FFTW_DIR=/global/software/rocky-8.x86_64/gcc/linux-rocky8-x86_64/gcc-13.2.0/fftw-3.3.10-nxbn4wosmyvhieaqrxfwflhckiys7qus/lib
-FFTW_INC=/global/software/rocky-8.x86_64/gcc/linux-rocky8-x86_64/gcc-13.2.0/fftw-3.3.10-nxbn4wosmyvhieaqrxfwflhckiys7qus/include
+FFTW_DIR=/global/home/users/rliu5/local/fftw-3.3.10/lib
 FFTWLIB      = $(FFTW_DIR)/libfftw3.so \
                $(FFTW_DIR)/libfftw3_threads.so \
-               $(FFTW_DIR)/libfftw3_mpi.so \
+               $(FFTW_DIR)/libfftw3_omp.so \
                ${CUDALIB}  -lstdc++
-FFTWINCLUDE  = $(FFTW_INC)
+FFTWINCLUDE  =/global/home/users/rliu5/local/fftw-3.3.10/include
+PERFORMANCE  = 
 
-HDF5_DIR=/global/software/rocky-8.x86_64/gcc/linux-rocky8-x86_64/gcc-13.2.0/hdf5-1.14.3-hd6ccvy36dh7u4tec27ikeghvbddhunk
-HDF5_LDIR    =  ${HDF5_DIR}/lib
+SCALAPACK_DIR=/global/home/users/rliu5/local/scalapack-2.2.2
+SCALAPACKLIB = -L${SCALAPACK_DIR}/lib -lscalapack
+
+LAPACK_DIR=/global/home/users/rliu5/local/lapack-3.12.1
+LAPACKLIB = -L${LAPACK_DIR}/lib64 -llapack -lblas
+#LAPACKLIB = -llapack -lblas 
+
+# # HDF5_LDIR    =  ${HDF5_DIR}/lib/
+# HDF5_LDIR    =  /global/homes/m/mdelben/LIBS_local/hdf5-1.10.5/lib/
+# HDF5LIB      =  $(HDF5_LDIR)/libhdf5hl_fortran.a \
+#                 $(HDF5_LDIR)/libhdf5_hl.a \
+#                 $(HDF5_LDIR)/libhdf5_fortran.a \
+#                 $(HDF5_LDIR)/libhdf5.a -lz -ldl
+# #HDF5INCLUDE  = ${HDF5_DIR}/include/
+# HDF5INCLUDE  = /global/homes/m/mdelben/LIBS_local/hdf5-1.10.5/include/
+
+HDF5_DIR=/global/home/users/rliu5/local/hdf5-1.14.3
+HDF5_LDIR    =  ${HDF5_DIR}/lib/
+# HDF5_LDIR    =  /pscratch/home/mdelben/LIBS_local/hdf5-1.10.5/lib/
 HDF5LIB      =  $(HDF5_LDIR)/libhdf5hl_fortran.a \
                 $(HDF5_LDIR)/libhdf5_hl.a \
                 $(HDF5_LDIR)/libhdf5_fortran.a \
                 $(HDF5_LDIR)/libhdf5.a -lz -ldl
-HDF5INCLUDE  = ${HDF5_DIR}/include
+HDF5INCLUDE  = ${HDF5_DIR}/include/
+# HDF5INCLUDE  = /pscratch/home/mdelben/LIBS_local/hdf5-1.10.5/include/
 
 
+# ELPALIB = PATH_TO_ELPA_DIR/lib/libelpa.a -lstdc++
+# ELPAINCLUDE = PATH_TO_ELPA_DIR/include/elpa-2021.11.001/modules/
