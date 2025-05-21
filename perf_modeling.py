d6136769b0ce29d4d7ca00de4cc5c3a304dc9951
csruiliu <rui.liu@proton.me>
2025-05-21 14:01:58 -0700

    add aggregation sample interval

--- perf_modeling.py
+++ perf_modeling.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import argparse
 import re
-import matplotlib.pyplot as plt
 
 
 # Define a custom argument type for a list of strings
@@ -36,6 +35,7 @@ def process_file(num_gpu, file_path, metric_names):
 
                 # Only get number values, so "GPU x" will be discarded
                 numeric_values = [float(value) for value in values if is_number(value)]
+
                 if len(numeric_values) == len(metric_names):
                     gpu_data[i].append(numeric_values)
                 else:
@@ -43,15 +43,15 @@ def process_file(num_gpu, file_path, metric_names):
                 
                 # Only one GPU per line, so stop checking patterns
                 break  
-    
+     
     gpu_dfs = [pd.DataFrame(gpu_data[i], columns=metric_names) for i in range(num_gpu)]
-    
+
     # returns a list of DataFrames, one per GPU
     return gpu_dfs  
 
     
 # Function to plot dataframe
-def perf_modeling_per_gpu(df, metrics, dcgm_delay, hw_pcie_gb, hw_nvlink_gb):
+def perf_modeling_per_gpu(df, metrics, sample_intv, hw_pcie_gb, hw_nvlink_gb):
     t_total_list = list()
     
     for row in df.itertuples(index=False, name='MetricRow'):
@@ -60,19 +60,19 @@ def perf_modeling_per_gpu(df, metrics, dcgm_delay, hw_pcie_gb, hw_nvlink_gb):
         # you can access row.GPUTL, row.SMACT, row.TENSO, etc.
         metric_values = list(getattr(row, metric) for metric in metrics)
         
-        t_flop = dcgm_delay * (metric_values[metrics.index('TENSO')] + metric_values[metrics.index('FP64A')] + 
+        t_flop = sample_intv * (metric_values[metrics.index('TENSO')] + metric_values[metrics.index('FP64A')] + 
                                metric_values[metrics.index('FP32A')] + metric_values[metrics.index('FP16A')])  
-        t_dram = dcgm_delay * metric_values[metrics.index('DRAMA')]
+        t_dram = sample_intv * metric_values[metrics.index('DRAMA')]
         
         t_roofline = max(t_flop, t_dram)
         
-        t_otherGPU = min(0, dcgm_delay * metric_values[metrics.index('GRACT')] - t_roofline)
+        t_otherGPU = max(0, sample_intv * metric_values[metrics.index('GRACT')] - t_roofline)
 
-        t_pcie = (metric_values[metrics.index('PCITX')] + metric_values[metrics.index('PCIRX')]) / (1024 * 1024 * hw_pcie_gb) * dcgm_delay 
+        t_pcie = (metric_values[metrics.index('PCITX')] + metric_values[metrics.index('PCIRX')]) * sample_intv / (1000 * 1000 * hw_pcie_gb) 
 
-        t_nvlink = (metric_values[metrics.index('NVLTX')] + metric_values[metrics.index('NVLRX')]) / (1024 * 1024 * hw_nvlink_gb) * dcgm_delay
+        t_nvlink = (metric_values[metrics.index('NVLTX')] + metric_values[metrics.index('NVLRX')]) * sample_intv / (1000 * 1000 * hw_nvlink_gb)
 
-        t_otherNode = min(0, dcgm_delay * (1 - metric_values[metrics.index('GRACT')]) - t_pcie - t_nvlink)
+        t_otherNode = max(0, sample_intv * (1 - metric_values[metrics.index('GRACT')]) - t_pcie - t_nvlink)
 
         t_total = t_roofline + t_otherGPU + t_pcie + t_nvlink + t_otherNode
 
@@ -81,11 +81,12 @@ def perf_modeling_per_gpu(df, metrics, dcgm_delay, hw_pcie_gb, hw_nvlink_gb):
     return t_total_list
 
 
-def perf_modeling(gpu_dfs, metrics, dcgm_delay, hw_pcie_gb, hw_nvlink_gb):
+def perf_modeling(gpu_dfs, metrics, sample_interval_ms, agg_interval_ms, hw_pcie_gb, hw_nvlink_gb):
+    sample_interval = sample_interval_ms / 1000
     t_total_dict = dict()
     for i, df in enumerate(gpu_dfs):
         if not df.empty:
-            t_totals = perf_modeling_per_gpu(df, metrics, dcgm_delay, hw_pcie_gb, hw_nvlink_gb)
+            t_totals = perf_modeling_per_gpu(df, metrics, sample_interval, hw_pcie_gb, hw_nvlink_gb)
             t_total_dict[f"GPU{i}"] = t_totals
         else:
             raise ValueError("The total time list is empty")
@@ -97,9 +98,22 @@ def perf_modeling(gpu_dfs, metrics, dcgm_delay, hw_pcie_gb, hw_nvlink_gb):
         raise ValueError("Not all GPU t_total lists are of the same length!")
     
     num_rows = lengths[0]
-    # Transpose the lists and take max per row index
-    max_list = [max(t_total_dict[gpu][row_idx] for gpu in t_total_dict) for row_idx in range(num_rows)]
-    print(sum(max_list) / 1000)
+    # Aggregate every `agg_samples` samples
+    agg_samples = agg_interval_ms // sample_interval_ms
+
+    # Transpose the lists and take max of every `agg_samples` samples
+    max_list = []
+    
+    for start in range(0, num_rows, agg_samples):
+        end = min(start + agg_samples, num_rows)
+        # For each row in this window, find the max across GPUs, then find the max in the window
+        window_max = max(
+            max(t_total_dict[gpu][row_idx] for gpu in t_total_dict)
+            for row_idx in range(start, end)
+        )
+        max_list.append(window_max)
+
+    print(sum(max_list))
 
 
 def main():
@@ -107,23 +121,24 @@ def main():
     # get all parameters
     ###################################
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('-f', '--dcgm_file', action='store', type=str,
+    parser.add_argument('-f', '--dcgm_file', action='store', type=str, required=True,
                         help='indicate the dcgm output file')
-    parser.add_argument('-n', '--num_gpu', action='store', type=int,
+    parser.add_argument('-n', '--num_gpu', action='store', type=int, required=True,
                         help='indicate number of gpus used for computation')
-    parser.add_argument('-d', '--dcgm_delay_ms', action='store', type=int,
-                        help='indicate the sample interval in milliseconds') 
-    parser.add_argument('--metrics', type=list_of_strings, help='List of metrics, basically the not-none col names')
+    parser.add_argument('-d', '--sample_interval_ms', action='store', type=int, required=True,
+                        help='indicate the sample interval in milliseconds')
+    parser.add_argument('-a', '--aggregate_interval_ms', action='store', type=int, required=True,
+                        help='indicate the time interval for aggregation in milliseconds') 
+    parser.add_argument('--metrics', type=list_of_strings, required=True, 
+                        help='List of metrics, basically the not-none col names')
     parser.add_argument('-h', '--help', action='help',
                         help='Example: python3 dcgm_analyze.py -f gpu_util/results/xx.100.out -o ./gpu_util/results -d 100 --metrics GRACT,PCITX,PCIRX')
-    # metric_cols = ["GPUTL", "SMACT", "TENSO", "DRAMA", "FP64A", "FP32A", "FP16A", "TIMMA", "THMMA"]
-    # metric_cols = ["GPUTL", "MCUTL", "GRACT", "PCITX", "PCIRX"]
-    # metric_cols = ["TMPTR", "POWER", "TOTEC", "GPUTL", "SMACT", "TENSO", "DRAMA", "FP64A", "FP32A", "FP16A", "TIMMA", "THMMA"]
     args = parser.parse_args()
 
     dcgm_metric_file = args.dcgm_file
     num_gpu = args.num_gpu
-    dcgm_delay_ms = args.dcgm_delay_ms
+    sample_interval_ms = args.sample_interval_ms
+    agg_interval_ms = args.aggregate_interval_ms
     metric_names = args.metrics
     
     hw_pcie_gb = 64
@@ -131,7 +146,8 @@ def main():
 
     profiled_results_df = process_file(num_gpu, dcgm_metric_file, metric_names)
     
-    perf_modeling(profiled_results_df, metric_names, dcgm_delay_ms, hw_pcie_gb, hw_nvlink_gb)
+    perf_modeling(profiled_results_df, metric_names, sample_interval_ms, agg_interval_ms, hw_pcie_gb, hw_nvlink_gb)
+
 
 if __name__=="__main__":
     main()
