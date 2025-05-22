a491934acb2e35f7cc685d07825f0bf74fc30859
csruiliu <rui.liu@proton.me>
2025-05-22 14:27:33 -0700

    adding end-to-end time profiling

--- gemm/scripts/run_gemm.sh
+++ gemm/scripts/run_gemm.sh
@@ -29,11 +29,15 @@ export DCGM_SAMPLE_RATE=100
 
 for prec in D S H; do
 #run the application:
+start=$(date +%s.%N)
 dcgm_delay=${DCGM_SAMPLE_RATE} \
 	srun -n 1 -c 1 --cpu_bind=cores -G 1 --gpu-bind=single:1 \
 	./wrap_dcgmi.sh \
 	./gemm.x 16384 100 1.0 1.0 $prec \
 	> ${RESULTS_DIR}/"$prec"gemm-${SLURM_JOBID}.dcgmi
+end=$(date +%s.%N)
+elapsed=$(printf "%s - %s\n" $end $start | bc -l)
+printf "Elapsed Time: %.2f seconds\n" $elapsed > ${RESULTS_DIR}/"$prec"gemm_d${DCGM_SAMPLE_RATE}_runtime.out
 done
 
 
